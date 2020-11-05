35177901e13c8044f12ffc5b94faa890c88bf878
Weifan Jiang <weifanjiang@hotmail.com>
2020-11-05 02:55:14 -0500

    is the service down

--- istheservicedown.py
+++ istheservicedown.py
@@ -8,17 +8,17 @@ import time
 from datetime import datetime
 
 def get_current_start():
-    with open("istheservicedown_march/config/start_time.json", "r") as fin:
+    with open("istheservicedown/config/start_time.json", "r") as fin:
         conf = json.load(fin)
     return conf["start_time"]
 
-with open("istheservicedown_march/config/2020-03-01.json", "r") as fin:
+with open("istheservicedown/config/2020-03-01.json", "r") as fin:
     conf = json.load(fin)
-    with open("istheservicedown_march/config/start_time.json", "w") as fout:
+    with open("istheservicedown/config/start_time.json", "w") as fout:
         json.dump(conf, fout)
 
 curr_start = get_current_start()
 while curr_start < 1588291200:  # 2020-05-01
-    os.system('python3 disqus_list_posts.py -s istheservicedown_march/config/start_time.json -o istheservicedown_march -c istheservicedown_march/config/istheservicedown.json')
+    os.system('python3 disqus_list_posts.py -s istheservicedown/config/start_time.json -o istheservicedown -c istheservicedown/config/istheservicedown.json')
     print("Sleeping for 70 min to avoid Quota, starting from {}".format(datetime.now().strftime("%m/%d/%Y, %H:%M:%S")))
     time.sleep(60 * 70)
