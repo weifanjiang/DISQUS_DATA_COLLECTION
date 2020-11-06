122378c2a4a0341ce79315b0ee6afe8ffec6d761
Weifan Jiang <weifanjiang@hotmail.com>
2020-11-06 02:07:07 -0500

    added istheservicedown data

--- istheservicedown.py
+++ istheservicedown.py
@@ -12,13 +12,13 @@ def get_current_start():
         conf = json.load(fin)
     return conf["start_time"]
 
-with open("istheservicedown/config/2020-03-01.json", "r") as fin:
+with open("istheservicedown/config/2019-12-30.json", "r") as fin:
     conf = json.load(fin)
     with open("istheservicedown/config/start_time.json", "w") as fout:
         json.dump(conf, fout)
 
 curr_start = get_current_start()
-while curr_start < 1588291200:  # 2020-05-01
+while curr_start < 1599004800:  # 2020-05-01
     os.system('python3 disqus_list_posts.py -s istheservicedown/config/start_time.json -o istheservicedown -c istheservicedown/config/istheservicedown.json')
     print("Sleeping for 70 min to avoid Quota, starting from {}".format(datetime.now().strftime("%m/%d/%Y, %H:%M:%S")))
     time.sleep(60 * 70)
